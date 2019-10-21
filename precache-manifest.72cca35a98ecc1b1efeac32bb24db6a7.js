b45009139125403795beb133665b7e8bbe145155
chris13012 <christophecaillet@icloud.com>
2019-10-21 11:53:25 +0200

    Updates

---
+++ precache-manifest.72cca35a98ecc1b1efeac32bb24db6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b243849912ccf81140ee578de474cac5",
+    "url": "/crud_hooks/index.html"
+  },
+  {
+    "revision": "73e07dd1f79b8571b235",
+    "url": "/crud_hooks/static/css/2.f6fac5cc.chunk.css"
+  },
+  {
+    "revision": "10061f98eb4203e615e1",
+    "url": "/crud_hooks/static/css/main.29d4c3f1.chunk.css"
+  },
+  {
+    "revision": "73e07dd1f79b8571b235",
+    "url": "/crud_hooks/static/js/2.94c2507e.chunk.js"
+  },
+  {
+    "revision": "10061f98eb4203e615e1",
+    "url": "/crud_hooks/static/js/main.3034f155.chunk.js"
+  },
+  {
+    "revision": "ab4cfe154e18038fabd2",
+    "url": "/crud_hooks/static/js/runtime-main.893c589b.js"
+  }
+]);
